ca961c0d552f495867b0a6b72a1da3f0b0dd27bd
AL <168145100+ALprjkt@users.noreply.github.com>
2024-07-08 09:56:57 +0700

    Update orion_ysl.mk

--- orion_ysl.mk
+++ orion_ysl.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 $(call inherit-product, device/xiaomi/ysl/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/orion/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Flags
 ORION_MAINTAINER := AL
